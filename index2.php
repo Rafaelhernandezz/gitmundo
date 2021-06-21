0e886605e7287ebb7f444225676efe929f7ff52f
091810677 rafael <rafahh531@gmail.com>
2021-06-21 00:56:19 -0500

    SNAPSHOT 002 DEL PROYECTO, SOLO MODIFICAMOS INDEX2

---
+++ index2.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:400,600" />
+    <link rel="stylesheet" href="css/all.min.css" />
+  
+    <link rel="stylesheet" href="css/templatemo-style.css" />
+
+
+
+
+    <link rel="stylesheet" href="css/bootstrap.min2.css">
+    <!-- style css -->
+    <link rel="stylesheet" href="css/style.css">
+    <!-- Responsive-->
+    <link rel="stylesheet" href="css/responsive.css">
+    <!-- fevicon -->
+    <link rel="icon" href="images/fevicon.png" type="image/gif" />
+    <!-- Scrollbar Custom CSS -->
+    
+
+
+
+    <title>PAGINA WEB RAFAEL</title>
+    
+
+  </head>
+  <body>
+  	 
+<font size="14" name= "Tempus Sans ITC" color="Blue">
+    
+
+   <marquee>
+    saludos cordiales TICSITOS 
+   </marquee>
+</font> 
+  </body>
+</html>
